42ab24faca575c13fd6fc11d3a56c143faec9ddd
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-11-12 13:57:17 +0700

    first-angular-project-done

---
+++ src/app/test-ngmodel/test-ngmodel.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-ngmodel',
+  templateUrl: './test-ngmodel.component.html',
+  styleUrls: ['./test-ngmodel.component.css']
+})
+export class TestNgmodelComponent implements OnInit {
+  product: string;
+  constructor() { 
+    this.product = 'hei';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+
+  name: string;
+  nim: string;
+
+  constructor() {
+    this.name = 'Raphael Gregorius Hakim'; // masukkan nama kamu
+    this.nim = '00000043340'; // masukkan nim kamu
+   }
+
+  ngOnInit(): void {
+  }
+
+}
